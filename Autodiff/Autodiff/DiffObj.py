61246f896aa20b83bf0bfc41214d5c819bd207dd
NickQianFeng <NickQian@NickQiandeMacBook-Air-2.local>
2018-10-17 07:23:15 -0400

    add support to poly

--- Autodiff/Autodiff/DiffObj.py
+++ Autodiff/Autodiff/DiffObj.py
@@ -1,9 +1,10 @@
 class DiffObj():
-	def __init__(self,name,obj_type='variable',name_list=None,val_left=0,val_right=0,der_left=0,der_right=0):
+	def __init__(self,name,obj_type='variable',name_list=None,val_left=0,val_right=0,der_left=0,der_right=0,degree=0):
 		if name_list==None:
 			self.name_list=[name]
 		else:
 			self.name_list=name_list
+		self.degree=degree
 		self.name=name
 		self.obj_type=obj_type
 		self.val_left=val_left
@@ -22,8 +23,9 @@ class DiffObj():
 	def vector():
 		pass
 	@staticmethod
-	def makePoly():
-		pass
+	def makePoly(term,degree=0):
+		return DiffObj(name='poly',obj_type='poly',name_list=term.name_list,val_left=term,degree=degree)
+		
 # /////////////////////////////////
 # operator overloads
 	def __add__(self,right):
@@ -32,16 +34,18 @@ class DiffObj():
 		pass
 # /////////////////////////////////
 # member functions
-	def get_var(self, name_dic={}):
+	def get_val(self, name_dic={}):
 		if self.obj_type=='add':
 			if set(self.name_list)==set(name_dic.keys()) or set(self.name_list) < set(name_dic.keys()):
-				return self.val_left.get_var(name_dic)+self.val_right.get_var(name_dic)
+				return self.val_left.get_val(name_dic)+self.val_right.get_val(name_dic)
 			else:
 				raise Exception('dic name error')
 		if self.obj_type=='scaler':
-			return slef.var_left
+			return self.var_left
 		if self.obj_type=='variable':
 			return name_dic[self.name]
+		if self.obj_type=='poly':
+			return pow(self.val_left.get_val(name_dic),self.degree)
 	def det_der(self):
 		pass
 
@@ -50,9 +54,20 @@ if __name__=='__main__':
 	y=DiffObj.variable('y')
 	z=DiffObj.variable('z')
 	w=x+y+z
-	print(w.get_var({'x':1,'y':1,'z':1}))
-	print(w.get_var({'x':2,'y':2,'z':2}))
-	print(w.get_var({'x':4,'y':4,'z':4}))
+	print(w.get_val({'x':1,'y':1,'z':1}))
+	print(w.get_val({'x':2,'y':2,'z':2}))
+	print(w.get_val({'x':3,'y':3,'z':3}))
+
+	j=DiffObj.variable('j')
+	j3=DiffObj.makePoly(j,degree=3)
+	print(j3.get_val({'j':1}))
+	print(j3.get_val({'j':2}))
+	print(j3.get_val({'j':3}))
+
+	j3andw=j3+x+y+z
+	print(j3andw.get_val({'x':1,'y':1,'z':1,'j':1}))
+	print(j3andw.get_val({'x':2,'y':2,'z':2,'j':2}))
+	print(j3andw.get_val({'x':3,'y':3,'z':3,'j':3}))
 
 	# print(z.get_der({'x':1,'y':1}))
 	# print(z.get_der({'x':2,'y':2}))
