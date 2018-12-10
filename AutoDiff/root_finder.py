9c3e0535d71ed806d6d876ad946e9287bec19993
Christy Cheng <christycheng@college.harvard.edu>
2018-12-09 23:09:38 -0500

    removed ifmain at end of file, all tests in /tests dir

--- AutoDiff/root_finder.py
+++ AutoDiff/root_finder.py
@@ -131,21 +131,3 @@ def quasi_newton(input_function,tolerance):
 
 def multiple_function_root_finder(input_function, lr=0.01, adadelta=False):
 	pass
-
-
-if __name__ == "__main__":
-	x=api.Variable('x')
-	y=api.Variable('y')
-
-	# answer is [0,2,-3]
-	single_function_to_solve=x**api.Constant('a_1', 3)+x**api.Constant('a_2', 2)-api.Constant('a_3', 6)*x
-	answer_for_single=single_function_root_finder(single_function_to_solve)
-	answer_for_single=single_function_root_finder(single_function_to_solve,adadelta=True)
-
-
-	# answer is [(x=1,y=4),(x=2,y=3)]
-	function_1=x*y-api.Constant('b_1', 4)*x-api.Constant('b_2', 2)*y+api.Constant('b_3', 8)
-	function_2=x*y-api.Constant('c_1', 3)*x-api.Constant('c_2', 1)*y+api.Constant('c_3', 3)
-	multiple_function_to_solve=[function_1,function_2]
-	answer_for_multiple=multiple_function_root_finder(multiple_function_to_solve)
-	answer_for_multiple=multiple_function_root_finder(multiple_function_to_solve,adadelta=True)
