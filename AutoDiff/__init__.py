8c1199cc04e3110d1a354900a3f8e4585dde9178
Abhimanyu Talwar <p10abhimanyut@iima.ac.in>
2018-12-11 16:44:12 -0500

    removed reference to Constant

--- AutoDiff/__init__.py
+++ AutoDiff/__init__.py
@@ -1,6 +1,5 @@
 from AutoDiff.AD import Variable
 from AutoDiff.AD import DiffObj
-from AutoDiff.AD import Constant
 from AutoDiff.AD import MathOps
 from AutoDiff.AD import VectorFunction
 import math
